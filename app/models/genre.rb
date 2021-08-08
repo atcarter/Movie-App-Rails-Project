8ad8557e5282731bff64c4f4786a024f0237c07a
Antonio <atcarter005@gmail.com>
2021-08-08 01:16:53 -0500

    Add genre validations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,12 @@
 class Genre < ApplicationRecord
   has_many :movies
+
+  validates :name, presence: true, uniqueness: true
+  before_validation :titlecase
+
+  private
+
+  def titlecase
+    self.name = self.name.titleize
+  end
 end
