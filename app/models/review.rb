87403173e610e1facde4486252aedc7b8d207b23
Antonio <atcarter005@gmail.com>
2021-08-08 01:20:14 -0500

    Add validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+
+  validates :rating, numericality: { less_than: 6, greater_than: 0 }
+  validates :content, length: { maximum: 500 }
 end
