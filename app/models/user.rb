b4691e8b8b6de797e76dacb1f31251d3244217f4
Antonio <atcarter005@gmail.com>
2021-08-08 01:14:46 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :movies, through: :reviews
 
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
   has_secure_password
 end
