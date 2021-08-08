91d498e451f5ea26f080656c683e3bfc4993139f
Antonio <atcarter005@gmail.com>
2021-08-08 01:13:47 -0500

    Add movie model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,15 @@ class Movie < ApplicationRecord
   belongs_to :genre
   has_many :reviews
   has_many :users, through: :reviews
+
+  validates :title, presence: true
+  validates :synopsis, length: { in: 20..300}
+  validates :year, numericality: { only_integer: true }
+  before_validation :titlecase
+
+  private
+
+  def titlecase
+    self.title = self.title.titleize
+  end
 end
