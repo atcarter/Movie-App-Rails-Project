1c0cee4bb2b1a4f7cfa461a44ed00f5dff5f15ca
Antonio <atcarter005@gmail.com>
2021-08-09 13:01:34 -0500

    Add routes to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    @user = User.find_by_id(params[:id])
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path
+    else
+      render :new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 end
