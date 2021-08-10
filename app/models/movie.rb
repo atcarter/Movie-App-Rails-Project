c91316800c290e66b4ecd45b434e60f7c7a529da
Antonio <atcarter005@gmail.com>
2021-08-09 22:44:15 -0500

    Add index page for movies. Add class method for displaying the average rating in the view.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,16 +8,16 @@ class Movie < ApplicationRecord
   validates :year, numericality: { only_integer: true }
   before_validation :titlecase
 
-  # def average_rating
-  #   sum = 0
-  #   count = 0
-  #   self.reviews.each |rev| do
-  #     sum += rev.rating
-  #     count += 1
-  #   end
-  #   avg = sum / count
-  #   avg
-  # end
+  def self.average_rating
+    sum = 0
+    count = 0
+    self.reviews.each do |rev|
+      sum += rev.rating
+      count += 1
+    end
+    avg = sum/count.to_f
+    avg
+  end
 
   private
 
