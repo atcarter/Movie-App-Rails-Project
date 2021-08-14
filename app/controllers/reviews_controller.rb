cf4f6ad3a5b54e864b540a32917628c1ff4138a0
Antonio <atcarter005@gmail.com>
2021-08-14 17:56:56 -0500

    Add index and new route to Reviews model.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  
+  def index
+    @movie = Movie.find_by_id(:movie_id)
+    @reviews = Review.order_by_rating
+  end
+
+  def new
+    @movie = Movie.find_by_id(:movie_id)
+    @review = Review.new
+  end
+
+  def create
+    
+  end
+
+
 end
