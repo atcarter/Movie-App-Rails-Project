4bf39270db61f68dabb6f737df8a8bfbd03ee42f
Antonio <atcarter005@gmail.com>
2021-08-14 20:15:45 -0500

    Remove scope from reviews model

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,8 +5,6 @@ class Review < ApplicationRecord
   validates :rating, numericality: { less_than: 6, greater_than: 0 }
   validates :content, length: { maximum: 500 }
 
-  scope :order_by_rating, -> { order(rating: :desc)}
-
   def stars
     "#{self.rating} Stars"
   end
