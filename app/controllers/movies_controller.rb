a14d1efd2c213f38836e72ade001b74f2bfd69eb
Antonio <atcarter005@gmail.com>
2021-08-14 20:45:52 -0500

    Add scope routes to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,14 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find_by_id(params[:id])
   end
+
+  def year_asc
+    @movies = Movie.order_by_year_asc
+    render :index
+  end
+
+  def year_desc
+    @movies = Movie.order_by_year_desc
+    render :index
+  end
 end
