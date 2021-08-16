6654370b82d1e887818fbf8d7eabf971c784f640
Antonio <atcarter005@gmail.com>
2021-08-16 05:30:29 -0500

    add a method to redirect if the user isn't the admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def redirect_if_not_signed_in?
     redirect_to login_path unless logged_in?
   end
+
+  def redirect_if_not_admin?
+    redirect_to movies_path unless admin?
+  end
 end
