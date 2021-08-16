4df0d64798417379c5ae56d73bad216b290cd24a
Antonio <atcarter005@gmail.com>
2021-08-16 04:56:19 -0500

    Add line for a movie to accept nested attributes for its genre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@ class Movie < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
+  accepts_nested_attributes_for :genre
+
   validates :title, presence: true
   validates :synopsis, length: { in: 20..300}
   validates :year, numericality: { only_integer: true }
